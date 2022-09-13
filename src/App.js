ec313c00e1b71bf13935ac329eb89df13e1cff0b
Tobias <juantobiasdl7@gmail.com>
2022-09-12 22:57:43 -0500

    I added the grid to the site and center my photo.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 import { Avatar, Wrap, Image } from '@chakra-ui/react';
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import go from './Untitled.jpg';
+import { Grid, GridItem, Flex, Spacer, Box, Text, Center, Heading } from '@chakra-ui/react';
+
 
 function App() {
   return (
-    <Wrap bg='#161E4A' >
-      <Tabs>
-        <TabList>
-          <Tab>One</Tab>
-          <Tab>Two</Tab>
-          <Tab>Three</Tab>
-        </TabList>
-
-        <TabPanels>
-          <TabPanel>
-            <p>one!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>three!</p>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    
-    
-      <Image
-        borderRadius='full'
-        boxSize='350px'
-        src= {go}
-        alt='Dan Abramov'
-      />
-    </Wrap>
+    <Grid
+      templateAreas={`"header1 header2 header3"
+                      "nav main main"
+                      "footer footer footer"`}
+      gridTemplateRows={'6vh 79vh 15vh'}
+      gridTemplateColumns={'33% 33% 33%'}
+      h='200px'
+      gap='1'
+      color='blackAlpha.700'
+      fontWeight='bold'
+    >
+    <GridItem pl='2' bg='#000000' area={'header1'}>
+      <Flex color='gray'>
+        <Box>
+          <Text>Box 3</Text>
+        </Box>
+        <Box >
+          <Text>Box 3</Text>
+        </Box>
+        <Box>
+          <Text>Box 3</Text>
+        </Box>
+      </Flex>
+    </GridItem>
+    <GridItem pl='2' bg='#000000' area={'header2'}>
+      <Flex  color='white'>
+        <Box mt='12px' w='100%' color='gray'>
+          <Center>
+            <Heading as='h1' size='lg' noOfLines={1}>Tobías De La Cruz</Heading>
+          </Center>
+        </Box>
+      </Flex>
+    </GridItem>
+    <GridItem pl='2' bg='#0F3460' area={'nav'}>
+      <Flex justify='center' align='center' h='100%'>
+        <Image
+            borderRadius='full'
+            boxSize='350px'
+            src= {go}
+            alt='Dan Abramov'
+            
+          />
+        </Flex>
+    </GridItem>
+    <GridItem pl='2' bg='#0F3460' area={'main'}>
+      Main
+    </GridItem>
+    <GridItem pl='2' bg='blue.300' area={'footer'}>
+      Footer
+    </GridItem>
+  </Grid>
+   
   );
 }
 
