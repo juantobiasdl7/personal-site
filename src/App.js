c9cf5e29c0d3d2739226acba0ca9041e368de1e3
Tobias <juantobiasdl7@gmail.com>
2022-09-13 21:09:58 -0500

    I changed colors, modified grid, updated copywrite.

--- src/App.js
+++ src/App.js
@@ -8,54 +8,61 @@ import { Grid, GridItem, Flex, Spacer, Box, Text, Center, Heading } from '@chakr
 function App() {
   return (
     <Grid
-      templateAreas={`"header1 header2 header3"
+      templateAreas={`"header1 header2 header2"
                       "nav main main"
+                      "nav2 main2 main2"
                       "footer footer footer"`}
-      gridTemplateRows={'6vh 79vh 15vh'}
+      gridTemplateRows={'5vh 45vh 45vh 10vh'}
       gridTemplateColumns={'33% 33% 33%'}
-      h='200px'
-      gap='1'
-      color='blackAlpha.700'
+      color='gray'
+      
       fontWeight='bold'
     >
-    <GridItem pl='2' bg='#000000' area={'header1'}>
-      <Flex color='gray'>
-        <Box>
-          <Text>Box 3</Text>
+    <GridItem pl='2' bg='#0c0d0d' area={'header1'}>
+      <Flex color='gray' h='100%' align='center'>
+        <Box ml='25px'>
+          <Text>About</Text>
         </Box>
-        <Box >
-          <Text>Box 3</Text>
+        <Box ml='25px'>
+          <Text>Twitter</Text>
         </Box>
-        <Box>
-          <Text>Box 3</Text>
+        <Box ml='25px'>
+          <Text>LinkedIn</Text>
         </Box>
       </Flex>
     </GridItem>
-    <GridItem pl='2' bg='#000000' area={'header2'}>
-      <Flex  color='white'>
-        <Box mt='12px' w='100%' color='gray'>
-          <Center>
+    <GridItem pl='2' bg='#0c0d0d' area={'header2'}>
+      <Flex h='100%' align='center' ml='10%'>
             <Heading as='h1' size='lg' noOfLines={1}>Tobías De La Cruz</Heading>
-          </Center>
-        </Box>
       </Flex>
     </GridItem>
-    <GridItem pl='2' bg='#0F3460' area={'nav'}>
+    <GridItem pl='2' bg='#101217' area={'nav'}>
       <Flex justify='center' align='center' h='100%'>
         <Image
             borderRadius='full'
-            boxSize='350px'
+            w='56%'
+            h='80%'
             src= {go}
             alt='Dan Abramov'
             
           />
         </Flex>
     </GridItem>
-    <GridItem pl='2' bg='#0F3460' area={'main'}>
-      Main
+    <GridItem pl='2' bg='#0c0d0d' area={'nav2'}>
+      
     </GridItem>
-    <GridItem pl='2' bg='blue.300' area={'footer'}>
-      Footer
+    <GridItem pl='2' bg='#101217' area={'main2'}>
+      
+    </GridItem>
+    <GridItem pl='2' bg='#0c0d0d' area={'main'}>
+      <Flex h='100%' direction='column' justify='center' >
+        <Heading as='h1' size='lg' noOfLines={1} ml='25%'>Hi there!</Heading>
+        <Text w='50%' ml='25%'>My name is Tobías. I'm a Mexican technology generalist, with recent experience as a Project Manager, remote Software Development teams and community building activities 🧑‍💻
+          I work as PM at Umvel. Highlights of my career include Aeroméxico, Global Hitss, Optimen... 🚀</Text>
+      </Flex>
+    </GridItem>
+    <GridItem pl='2' bg='#0c0d0d' area={'footer'}>
+    Copyright © 2022, Tobías De La Cruz. 
     </GridItem>
   </Grid>
    
