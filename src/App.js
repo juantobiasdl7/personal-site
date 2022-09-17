f4f9b02d150a2af4da156a6427b1b1c45bd4fd75
Tobias <juantobiasdl7@gmail.com>
2022-09-16 23:41:46 -0500

    I push the first version of the site, this version only looks good on  mobile.

--- src/App.js
+++ src/App.js
@@ -1,72 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
 import { Avatar, Wrap, Image } from '@chakra-ui/react';
 import go from './Untitled.jpg';
-import { Grid, GridItem, Flex, Spacer, Box, Text, Center, Heading } from '@chakra-ui/react';
+import { Grid, GridItem, Flex, Spacer, Box, Text, Center, Heading, IconButton, Link, Button, FormControl, Input, Textarea} from '@chakra-ui/react';
+import { Icon } from '@chakra-ui/react'
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+
+function LinkedIn() {
+  return <Icon as={AiFillLinkedin} />
+}
+
+function Github() {
+  return <Icon as={AiFillGithub} />
+}
 
 
 function App() {
   return (
-    <Grid
-      templateAreas={`"header1 header2 header2"
-                      "nav main main"
-                      "nav2 main2 main2"
-                      "footer footer footer"`}
-      gridTemplateRows={'5vh 45vh 45vh 10vh'}
-      gridTemplateColumns={'33% 33% 33%'}
-      color='gray'
-      
-      fontWeight='bold'
-    >
-    <GridItem pl='2' bg='#0c0d0d' area={'header1'}>
-      <Flex color='gray' h='100%' align='center'>
-        <Box ml='25px'>
-          <Text>About</Text>
-        </Box>
-        <Box ml='25px'>
-          <Text>Twitter</Text>
-        </Box>
-        <Box ml='25px'>
-          <Text>LinkedIn</Text>
-        </Box>
-      </Flex>
-    </GridItem>
-    <GridItem pl='2' bg='#0c0d0d' area={'header2'}>
-      <Flex h='100%' align='center' ml='10%'>
-            <Heading as='h1' size='lg' noOfLines={1}>Tobías De La Cruz</Heading>
-      </Flex>
-    </GridItem>
-    <GridItem pl='2' bg='#101217' area={'nav'}>
-      <Flex justify='center' align='center' h='100%'>
-        <Image
-            borderRadius='full'
-            w='56%'
-            h='80%'
-            src= {go}
-            alt='Dan Abramov'
-            
-          />
+
+      <Box>
+        <Flex color='gray' h='50px' align='center' bg='#0c0d0d' w='100%' pos='fixed' >
+          <Box ml='25px'>
+            <Text>About</Text>
+          </Box>
+          <Box ml='25px'>
+            <Text>Twitter</Text>
+          </Box>
+          <Box ml='25px'>
+            <Text>LinkedIn</Text>
+          </Box>
         </Flex>
-    </GridItem>
-    <GridItem pl='2' bg='#0c0d0d' area={'nav2'}>
-      
-    </GridItem>
-    <GridItem pl='2' bg='#101217' area={'main2'}>
-      
-    </GridItem>
-    <GridItem pl='2' bg='#0c0d0d' area={'main'}>
-      <Flex h='100%' direction='column' justify='center' >
-        <Heading as='h1' size='lg' noOfLines={1} ml='25%'>Hi there!</Heading>
-        <Text w='50%' ml='25%'>My name is Tobías. I'm a Mexican technology generalist, with recent experience as a Project Manager, remote Software Development teams and community building activities 🧑‍💻
-          I work as PM at Umvel. Highlights of my career include Aeroméxico, Global Hitss, Optimen... 🚀</Text>
-      </Flex>
-    </GridItem>
-    <GridItem pl='2' bg='#0c0d0d' area={'footer'}>
-    Copyright © 2022, Tobías De La Cruz. 
-    </GridItem>
-  </Grid>
-   
-  );
+
+        <Flex w='100%' wrap='wrap' pt='50px'>
+          <Box w='100%' h='300px' bg='black'>
+            <Flex w='100%' h='100%' justify='center' align='center'>
+              <Image
+                    borderRadius='full'
+                    boxSize='250px'
+                    src= {go}
+                    alt='Tobías De La Cruz'
+              />
+            </Flex>
+          </Box>
+          <Box w='100%' h='fit-content' bg='#0c0d0d' color='gray' pb='10px'>
+            <Heading as='h1' size='lg' noOfLines={1} ml='25%' mt='20px'>Hi there!</Heading>
+             <Text w='50%' ml='25%'>My name is Tobías. I'm a Mexican technology generalist, with recent experience as a Project Manager, remote Software Development teams and community building activities 🧑‍💻
+             I work as PM at Umvel. Highlights of my career include Aeroméxico, Global Hitss, Optimen... 🚀</Text>
+          </Box>
+          <Box w='100%' h='fit-content' bg='black' color='gray' pb='10px'>
+          <FormControl>
+            <Flex direction='column' align='center'>
+              <Center >
+                <Text mt='12px' mb='20px' fontSize='4xl'>Contact me</Text>
+              </Center>
+              <Input w='80%' mb='20px' placeholder='Name' />
+              <Input w='80%' mb='20px' type='email' placeholder='Email' />
+              <Input w='80%' mb='20px' placeholder='Subject' />
+              <Textarea w='80%' placeholder='Message' h='89%' />
+                <Button
+                mt={4}
+                color='black'
+                variant='solid'
+                type='submit'
+              >
+                  Submit
+              </Button>
+              <Box pt='10px' w='40%'>
+                <Flex>
+                  <Link href='https://www.linkedin.com/in/tob%C3%ADas/'>
+                    <IconButton
+                      variant='outline'
+                      colorScheme='white'
+                      aria-label='Call Sage'
+                      fontSize='20px'
+                      icon={<LinkedIn/>}
+                    />
+                  </Link>
+                  <Spacer/>
+                  <Link href='https://github.com/juantobiasdl7'>
+                    <IconButton
+                      variant='outline'
+                      colorScheme='white'
+                      aria-label='Call Sage'
+                      fontSize='20px'
+                      href=''
+                      icon={<Github/>}
+                    />
+                  </Link>
+                </Flex>
+              </Box>
+              </Flex>
+            </FormControl>
+
+          </Box>
+          <Box w='100%' h='50px' bg='#0c0d0d' color='gray'>
+            <Center >
+              <Text mt='12px' fontSize='sm'>Copyright © 2022, Tobías De La Cruz.  </Text>
+            </Center>
+          </Box>
+        </Flex>    
+      </Box>
+    
+  )
 }
 
 export default App;
